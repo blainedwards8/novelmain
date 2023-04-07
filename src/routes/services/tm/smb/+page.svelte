73c670add97823f58e197d0fc79dcc3558c15395
Blaine Edwards <blainedwards8@gmail.com>
2023-04-07 13:37:25 -0600

    Added smb tm

---
+++ src/routes/services/tm/smb/+page.svelte
@@ -0,0 +1,70 @@
+<script>
+    import ServicePage from "/src/components/service-page.svelte";
+    let pTitle =
+      "Novel Law Group - Helping Small Business Protect their Brands with Trademarks";
+    let description = "Small Business Brand Protection with Trademarks";
+  </script>
+  
+  <svelte:head>
+    <title>{pTitle}</title>
+    <meta name="description" content={description} />
+    <meta property="og:title" content={pTitle} />
+    <meta property="og:description" content={description} />
+    <meta property="og:image" content="https://www.novel.law/img/nlg.png" />
+    <meta property="og:url" content="https://www.novel.law/" />
+    <meta property="og:site_name" content="Novel Law Group" />
+    <meta property="og:type" content="website" />
+    <meta name="twitter:card" content="summary" />
+    <meta name="twitter:title" content={pTitle} />
+    <meta name="twitter:description" content={description} />
+    <meta name="twitter:image" content="https://www.novel.law/img/nlg.png" />
+    <meta name="twitter:site" content="@novellawgroup" />
+    <meta name="twitter:creator" content="@novellawgroup" />
+  </svelte:head>
+  
+  <ServicePage title="How can you protect your brand with trademarks" tagline="A novel approach to helping you protect your brands with trademarks" />
+  
+  <div class="content">
+    <h1>Trademark Protection for Small Businesses: Why It Matters and How to Get It</h1>
+    For small businesses, establishing a strong brand is essential for standing out in a crowded market and building a loyal customer base. But what happens if someone else tries to use your brand name, logo, or other identifying features? That's where trademark protection comes in. In this article, we'll discuss why trademark protection matters for small businesses and how you can get it.
+  </div>
+  <div class="content">
+    <h2>What is a Trademark?</h2>
+    A trademark is a word, phrase, symbol, or design that identifies and distinguishes the source of a particular product or service. Trademarks are important because they help consumers recognize and trust your brand, and they provide legal protection against others using similar identifying features.
+  </div>
+  
+  <div class="content">
+    <h2>Why Trademark Protection Matters for Small Businesses</h2>
+    Trademark protection is particularly important for small businesses because they often have limited resources and face intense competition from larger, established companies. Here are a few reasons why trademark protection matters for small businesses:
+  </div>
+  <div class="content">
+  <ol>
+    <li><b>Protects Your Brand:</b> A trademark gives you the exclusive right to use your brand name, logo, and other identifying features, and prevents others from using them without your permission.</li>
+    <li><b>Builds Trust with Customers:</b> When customers see your trademark, they know they're getting a product or service from your company and not a competitor. This builds trust and loyalty with your customers.</li>
+    <li><b>Avoids Confusion:</b> Trademark protection prevents confusion among customers and helps you avoid legal disputes with competitors.</li>
+  </ol>
+  </div>
+  
+  <div class="content">
+    <h2>How to Get Trademark Protection</h2>
+    Getting trademark protection is a multi-step process, but it's essential for protecting your brand and business. Here's what you need to do:
+  </div>
+  
+  <div class="content">
+  <ol>
+    <li><b>Conduct a Trademark Search:</b> Before you can apply for a trademark, you need to make sure no one else has already registered a similar trademark. You can do this by conducting a trademark search, either on your own or with the help of a trademark attorney.</li>
+    <li><b>File a Trademark Application:</b> Once you've conducted a trademark search and determined that your proposed trademark is available, you can file a trademark application with the U.S. Patent and Trademark Office (USPTO). The application process can take several months or longer, and it's important to work with an experienced trademark attorney to ensure your application is properly drafted and submitted.</li>
+    <li><b>Monitor Your Trademark:</b> After your trademark is registered, it's important to monitor it to ensure no one else is using a similar trademark. You can do this on your own or with the help of a trademark monitoring service.</li>
+  </ol>
+  </div>
+  
+  <style>
+    .content {
+      @apply px-60;
+      @apply mt-4;
+    }
+
+    h1,h2{
+      @apply text-3xl;
+    }
+  </style>
